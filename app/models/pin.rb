5f8573e5ca36d66e379ae7d83a37f2cf9d358153
Ron Lawson <ronlawson@gmail.com>
2015-06-14 16:40:42 -0400

    Add attr_accessor to pins model due to heroku error

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,6 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
+	attr_accessible :image
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
