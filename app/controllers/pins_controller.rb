0039851e17ebc4669f1f21643fad2fa8b75fdc11
Ron Lawson <ronlawson@gmail.com>
2015-06-14 21:53:48 -0400

    Add pagination to pins index

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,7 +4,7 @@ class PinsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @pins = Pin.all.reverse_order
+    @pins = Pin.all.reverse_order.paginate(:page => params[:page], :per_page => 8)
   end
 
   def show
